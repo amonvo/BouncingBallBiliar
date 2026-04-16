cb85aba212c858a9fc986a3a91ff6fceeb4cd378
amonv <vojtech.amon@seznam.cz>
2026-04-17 00:03:55 +0200

    ﻿Refactor: multi-ball simulation with dark UI toolbar
    Add Ball.cs, rewrite MainForm with List<Ball>, wall + ball-to-ball collisions, dark theme toolbar (speed/ball-count trackbars, pause, reset, bounce counter), remove Designer.cs and resx.

---
+++ KulecnikAmon/Ball.cs
@@ -0,0 +1,44 @@
+/*
+ * Vytvoøeno aplikací SharpDevelop.
+ * Uživatel: amonv
+ */
+using System.Drawing;
+
+namespace KulecnikAmon
+{
+    /// <summary>
+    /// Represents a single bouncing ball.
+    /// </summary>
+    public class Ball
+    {
+        /// <summary>Top-left X position (pixels, sub-pixel precision).</summary>
+        public double X { get; set; }
+
+        /// <summary>Top-left Y position (pixels, sub-pixel precision).</summary>
+        public double Y { get; set; }
+
+        /// <summary>Horizontal velocity in pixels per tick (base, before speed multiplier).</summary>
+        public double VelocityX { get; set; }
+
+        /// <summary>Vertical velocity in pixels per tick (base, before speed multiplier).</summary>
+        public double VelocityY { get; set; }
+
+        /// <summary>Diameter in pixels (20–60).</summary>
+        public int Size { get; set; }
+
+        /// <summary>Fill colour of the ball.</summary>
+        public Color BallColor { get; set; }
+
+        /// <summary>Number of times this ball has bounced off a wall.</summary>
+        public int BounceCount { get; set; }
+
+        /// <summary>Convenience: radius = Size / 2.0</summary>
+        public double Radius { get { return Size / 2.0; } }
+
+        /// <summary>Centre X coordinate.</summary>
+        public double CenterX { get { return X + Radius; } }
+
+        /// <summary>Centre Y coordinate.</summary>
+        public double CenterY { get { return Y + Radius; } }
+    }
+}
--- KulecnikAmon/MainForm.cs
+++ KulecnikAmon/MainForm.cs
@@ -1,182 +1,328 @@
-﻿/*
- * Vytvořeno aplikací SharpDevelop.
- * Uživatel: amonv
+/*
+ * Vytvo�eno aplikac� SharpDevelop.
+ * U�ivatel: amonv
  * Datum: 11.11.2022
- * Čas: 1:13
- * 
- * Tento template můžete změnit pomocí Nástroje | Možnosti | Psaní kódu | Upravit standardní hlavičky souborů.
  */
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace KulecnikAmon
 {
-	/// <summary>
-	/// Description of MainForm.
-	/// </summary>
-public partial class MainForm : Form
-	{
-        private const int BallSize = 50;
-        private const int DefaultMoveStep = 2;
-        private const int MinSpeed = 1;
-        private const int MaxSpeed = 20;
-        private const int DefaultSpeed = 1;
-        private const int DefaultColorIndex = 2;
-        private const int DefaultBallX = 200;
-        private const int DefaultBallY = 150;
-		
-		private int _ballWidth = BallSize;
-		private int _ballHeight = BallSize;
-        private long _ballPosX = DefaultBallX;
-        private long _ballPosY = DefaultBallY;
-		private int _moveStepX = DefaultMoveStep;
-		private int _moveStepY = DefaultMoveStep;
-		private int _speed = DefaultSpeed;
-		private Brush _ballBrush = Brushes.Green;
-	
-		
-		public MainForm()
-		{
-			InitializeComponent();
-            progressBar1.Maximum = MaxSpeed;
-			
-			SetStyle(ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint, true);
-			
-			
-		UpdateStyles();
-		
-        colorSelect.SelectedIndex = DefaultColorIndex;
-	speedSelect.SelectedIndex = 0;
-	progressBar1.Value = DefaultSpeed;
-		}
-    private void MainForm_Paint(object sender, PaintEventArgs e)
-		{
-			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
-				
-			e.Graphics.Clear(BackColor);
-			
-            e.Graphics.FillEllipse(_ballBrush, _ballPosX, _ballPosY, _ballWidth, _ballHeight);
-			
-            using (Pen outlinePen = new Pen(Color.Black, 2))
-{
-e.Graphics.DrawEllipse(outlinePen, _ballPosX, _ballPosY, _ballWidth, _ballHeight);
-}
-
-using (SolidBrush highlightBrush = new SolidBrush(Color.FromArgb(80, 255, 255, 255)))
-{
-e.Graphics.FillEllipse(highlightBrush, _ballPosX + 10, _ballPosY + 5, _ballWidth / 3, _ballHeight / 3);
-}
-		}
-
-    private void Timer_Tick(object sender, EventArgs e)
-		{
-	_ballPosX += _moveStepX * _speed;
-			
-	if (_ballPosX < 0 ||
-	    _ballPosX + _ballWidth > ClientSize.Width) {
-		_moveStepX = -_moveStepX;
-			MoveColor();
-		}
-	_ballPosY += _moveStepY * _speed;
-	if (_ballPosY < 0 ||
-	    _ballPosY + _ballHeight > ClientSize.Height) 
-	{
-		_moveStepY = -_moveStepY;
-			MoveColor();
-		}
-		Invalidate();
-	}
-		
-    private void SpeedSelect_SelectedIndexChanged(object sender, EventArgs e)
+    public class MainForm : Form
     {
-	if (int.TryParse(speedSelect.Text, out int parsedSpeed))
-	{
-	if (parsedSpeed >= MinSpeed && parsedSpeed <= MaxSpeed)
-	{
-	_speed = parsedSpeed;
-	progressBar1.Value = parsedSpeed;
-	}
-	}
-    }
-		
-        private void MoveColor() 
+        // ?? constants ?????????????????????????????????????????????????????????????
+        private const int    MinSpeed    = 1;
+        private const int    MaxSpeed    = 20;
+        private const double MinVelocity = 1.0;
+        private const double MaxVelocity = 4.0;
+
+        private static readonly Color[] BallPalette =
         {
-		int actualColor = colorSelect.SelectedIndex;
-			
-		if (actualColor < colorSelect.Items.Count - 1) {
-				actualColor++;				
-			} else {
-				actualColor = 0;
-			}	
-			
-			colorSelect.SelectedIndex = actualColor;			
-		this.ChangeColor(colorSelect.Text);			
-		}
-		
-    private void ChangeColor(string colorName)
-    {
-	switch (colorName)
-	{
-	case "modrá":
-	_ballBrush = Brushes.Blue;
-	break;
-	case "červená":
-	_ballBrush = Brushes.Red;
-	break;
-	case "zelená":
-	_ballBrush = Brushes.Green;
-	break;
-	case "žlutá":
-	_ballBrush = Brushes.Yellow;
-	break;
-	case "fialová":
-	_ballBrush = Brushes.Purple;
-	break;
-	case "černá":
-	_ballBrush = Brushes.Black;
-	break;
-	case "oranžová":
-	_ballBrush = Brushes.Orange;
-	break;
-	case "hnědá":
-	_ballBrush = Brushes.Brown;
-	break;
-	case "růžová":
-	_ballBrush = Brushes.Pink;
-	break;
-	case "azurová":
-	_ballBrush = Brushes.Cyan;
-	break;
-	default:
-	_ballBrush = Brushes.Green;
-	break;
-	}
-    }
-		
-    private void ColorSelect_SelectedIndexChanged(object sender, EventArgs e)
-    {
-	ChangeColor(colorSelect.Text);
-	Invalidate();
-    }
-		
-    private void ProgressBar1_Click(object sender, EventArgs e)
-	{			
-		float absoluteMouse = (PointToClient(MousePosition).X - progressBar1.Bounds.X);
-		float calcFactor = progressBar1.Width / (float)progressBar1.Maximum;
-		float relativeMouse = absoluteMouse / calcFactor;
+            Color.Red, Color.DodgerBlue, Color.Lime, Color.Orange,
+            Color.MediumPurple, Color.Cyan, Color.HotPink, Color.Gold
+        };
 
-		int newSpeed = Convert.ToInt32(relativeMouse);
-		
-	if (newSpeed < MinSpeed)
-		newSpeed = MinSpeed;
-	if (newSpeed > MaxSpeed)
-		newSpeed = MaxSpeed;
-		
-        _speed = progressBar1.Value = newSpeed;
-		if (newSpeed > 0 && newSpeed <= speedSelect.Items.Count)
-speedSelect.SelectedIndex = newSpeed - 1; 
-	}
-	}
+        // ?? infrastructure ????????????????????????????????????????????????????????
+        private IContainer components = null;
+        private Timer      timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null)
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        // ?? simulation fields ??????????????????????????????????????????????????????
+        private List<Ball> _balls  = new List<Ball>();
+        private int        _speed  = 5;
+        private bool       _paused = false;
+        private Random     _rng    = new Random();
+
+        // ?? toolbar controls ???????????????????????????????????????????????????????
+        private Label    lblSpeed;
+        private TrackBar speedTrackBar;
+        private Label    lblSpeedVal;
+        private Label    lblBalls;
+        private TrackBar ballTrackBar;
+        private Label    lblBallCount;
+        private Button   pauseButton;
+        private Button   resetButton;
+        private Label    totalBouncesLabel;
+
+        // ?? constructor ????????????????????????????????????????????????????????????
+        public MainForm()
+        {
+            components = new Container();
+
+            // Timer
+            timer1          = new Timer(components);
+            timer1.Interval = 10;
+            timer1.Tick    += Timer_Tick;
+            timer1.Enabled  = true;
+
+            // Form properties
+            this.Text             = "Bouncing Ball Simulator";
+            this.BackColor        = Color.FromArgb(30, 30, 30);
+            this.ClientSize       = new System.Drawing.Size(1034, 537);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode    = AutoScaleMode.Font;
+            this.Paint           += MainForm_Paint;
+
+            SetStyle(
+                ControlStyles.OptimizedDoubleBuffer |
+                ControlStyles.AllPaintingInWmPaint  |
+                ControlStyles.UserPaint, true);
+            UpdateStyles();
+
+            // ?? Speed label ???????????????????????????????????????????????????????
+            lblSpeed           = new Label();
+            lblSpeed.Text      = "Speed:";
+            lblSpeed.ForeColor = Color.White;
+            lblSpeed.Location  = new System.Drawing.Point(10, 12);
+            lblSpeed.AutoSize  = true;
+
+            // ?? Speed trackbar ????????????????????????????????????????????????????
+            speedTrackBar               = new TrackBar();
+            speedTrackBar.Minimum       = MinSpeed;
+            speedTrackBar.Maximum       = MaxSpeed;
+            speedTrackBar.Value         = 5;
+            speedTrackBar.Width         = 180;
+            speedTrackBar.Height        = 30;
+            speedTrackBar.Location      = new System.Drawing.Point(65, 8);
+            speedTrackBar.TickFrequency = 5;
+            speedTrackBar.ValueChanged += SpeedTrackBar_ValueChanged;
+
+            // ?? Speed value label ?????????????????????????????????????????????????
+            lblSpeedVal           = new Label();
+            lblSpeedVal.Text      = "5";
+            lblSpeedVal.ForeColor = Color.White;
+            lblSpeedVal.Width     = 25;
+            lblSpeedVal.Location  = new System.Drawing.Point(250, 12);
+
+            // ?? Balls label ???????????????????????????????????????????????????????
+            lblBalls           = new Label();
+            lblBalls.Text      = "Balls:";
+            lblBalls.ForeColor = Color.White;
+            lblBalls.Location  = new System.Drawing.Point(290, 12);
+            lblBalls.AutoSize  = true;
+
+            // ?? Ball count trackbar ???????????????????????????????????????????????
+            ballTrackBar               = new TrackBar();
+            ballTrackBar.Minimum       = 1;
+            ballTrackBar.Maximum       = 10;
+            ballTrackBar.Value         = 5;
+            ballTrackBar.Width         = 120;
+            ballTrackBar.Height        = 30;
+            ballTrackBar.Location      = new System.Drawing.Point(335, 8);
+            ballTrackBar.TickFrequency = 1;
+            ballTrackBar.ValueChanged += BallTrackBar_ValueChanged;
+
+            // ?? Ball count value label ????????????????????????????????????????????
+            lblBallCount           = new Label();
+            lblBallCount.Text      = "5";
+            lblBallCount.ForeColor = Color.White;
+            lblBallCount.Width     = 20;
+            lblBallCount.Location  = new System.Drawing.Point(460, 12);
+
+            // ?? Pause button ??????????????????????????????????????????????????????
+            pauseButton           = new Button();
+            pauseButton.Text      = "\u23F8 Pause";
+            pauseButton.Width     = 90;
+            pauseButton.Height    = 28;
+            pauseButton.Location  = new System.Drawing.Point(495, 9);
+            pauseButton.FlatStyle = FlatStyle.Flat;
+            pauseButton.BackColor = Color.FromArgb(60, 60, 60);
+            pauseButton.ForeColor = Color.White;
+            pauseButton.Click    += PauseButton_Click;
+
+            // ?? Reset button ??????????????????????????????????????????????????????
+            resetButton           = new Button();
+            resetButton.Text      = "\u21BA Reset";
+            resetButton.Width     = 80;
+            resetButton.Height    = 28;
+            resetButton.Location  = new System.Drawing.Point(592, 9);
+            resetButton.FlatStyle = FlatStyle.Flat;
+            resetButton.BackColor = Color.FromArgb(60, 60, 60);
+            resetButton.ForeColor = Color.White;
+            resetButton.Click    += ResetButton_Click;
+
+            // ?? Total bounces label ???????????????????????????????????????????????
+            totalBouncesLabel           = new Label();
+            totalBouncesLabel.Text      = "Bounces: 0";
+            totalBouncesLabel.ForeColor = Color.White;
+            totalBouncesLabel.Width     = 130;
+            totalBouncesLabel.Location  = new System.Drawing.Point(690, 12);
+            totalBouncesLabel.AutoSize  = false;
+
+            this.Controls.Add(lblSpeed);
+            this.Controls.Add(speedTrackBar);
+            this.Controls.Add(lblSpeedVal);
+            this.Controls.Add(lblBalls);
+            this.Controls.Add(ballTrackBar);
+            this.Controls.Add(lblBallCount);
+            this.Controls.Add(pauseButton);
+            this.Controls.Add(resetButton);
+            this.Controls.Add(totalBouncesLabel);
+
+            InitializeBalls(5);
+        }
+
+        // ?? ball initialisation ????????????????????????????????????????????????????
+        private void InitializeBalls(int count)
+        {
+            _balls.Clear();
+
+            int areaW = ClientSize.Width  > 100 ? ClientSize.Width  : 800;
+            int areaH = ClientSize.Height > 100 ? ClientSize.Height : 537;
+
+            for (int i = 0; i < count; i++)
+            {
+                int size = _rng.Next(20, 56);
+
+                double vx = MinVelocity + _rng.NextDouble() * (MaxVelocity - MinVelocity);
+                double vy = MinVelocity + _rng.NextDouble() * (MaxVelocity - MinVelocity);
+                if (_rng.Next(2) == 0) vx = -vx;
+                if (_rng.Next(2) == 0) vy = -vy;
+
+                _balls.Add(new Ball
+                {
+                    X           = _rng.Next(0, Math.Max(1, areaW - size)),
+                    Y           = _rng.Next(45, Math.Max(46, areaH - size)),
+                    VelocityX   = vx,
+                    VelocityY   = vy,
+                    Size        = size,
+                    BallColor   = BallPalette[i % BallPalette.Length],
+                    BounceCount = 0
+                });
+            }
+        }
+
+        // ?? painting ??????????????????????????????????????????????????????????????
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            e.Graphics.Clear(Color.FromArgb(30, 30, 30));
+
+            foreach (Ball ball in _balls)
+                DrawBall(e.Graphics, ball);
+        }
+
+        private void DrawBall(Graphics g, Ball ball)
+        {
+            float x    = (float)ball.X;
+            float y    = (float)ball.Y;
+            float size = (float)ball.Size;
+
+            using (SolidBrush fill = new SolidBrush(ball.BallColor))
+                g.FillEllipse(fill, x, y, size, size);
+
+            using (Pen outline = new Pen(Color.Black, 2))
+                g.DrawEllipse(outline, x, y, size, size);
+
+            using (SolidBrush highlight = new SolidBrush(Color.FromArgb(80, 255, 255, 255)))
+                g.FillEllipse(highlight, x + 8f, y + 5f, size / 3f, size / 3f);
+        }
+
+        // ?? timer tick ?????????????????????????????????????????????????????????????
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (_paused) return;
+
+            // Move every ball and handle wall bounces.
+            foreach (Ball ball in _balls)
+            {
+                ball.X += ball.VelocityX * _speed * 0.25;
+                ball.Y += ball.VelocityY * _speed * 0.25;
+
+                if (ball.X < 0)
+                {
+                    ball.X         = 0;
+                    ball.VelocityX = -ball.VelocityX;
+                    ball.BounceCount++;
+                }
+                else if (ball.X + ball.Size > ClientSize.Width)
+                {
+                    ball.X         = ClientSize.Width - ball.Size;
+                    ball.VelocityX = -ball.VelocityX;
+                    ball.BounceCount++;
+                }
+
+                if (ball.Y < 45)
+                {
+                    ball.Y         = 45;
+                    ball.VelocityY = -ball.VelocityY;
+                    ball.BounceCount++;
+                }
+                else if (ball.Y + ball.Size > ClientSize.Height)
+                {
+                    ball.Y         = ClientSize.Height - ball.Size;
+                    ball.VelocityY = -ball.VelocityY;
+                    ball.BounceCount++;
+                }
+            }
+
+            // Ball-to-ball collision detection (elastic approximation: swap velocities).
+            for (int i = 0; i < _balls.Count - 1; i++)
+            {
+                for (int j = i + 1; j < _balls.Count; j++)
+                {
+                    Ball a = _balls[i];
+                    Ball b = _balls[j];
+
+                    double ax   = a.X + a.Size / 2.0;
+                    double ay   = a.Y + a.Size / 2.0;
+                    double bx   = b.X + b.Size / 2.0;
+                    double by   = b.Y + b.Size / 2.0;
+                    double dist = Math.Sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+                    double minD = (a.Size + b.Size) / 2.0;
+
+                    if (dist < minD && dist > 0)
+                    {
+                        double tmpVX = a.VelocityX;
+                        double tmpVY = a.VelocityY;
+                        a.VelocityX  = b.VelocityX;
+                        a.VelocityY  = b.VelocityY;
+                        b.VelocityX  = tmpVX;
+                        b.VelocityY  = tmpVY;
+                    }
+                }
+            }
+
+            totalBouncesLabel.Text = "Bounces: " + _balls.Sum(b => b.BounceCount).ToString();
+            Invalidate();
+        }
+
+        // ?? toolbar event handlers ?????????????????????????????????????????????????
+        private void SpeedTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            _speed           = speedTrackBar.Value;
+            lblSpeedVal.Text = _speed.ToString();
+        }
+
+        private void BallTrackBar_ValueChanged(object sender, EventArgs e)
+        {
+            InitializeBalls(ballTrackBar.Value);
+            lblBallCount.Text = ballTrackBar.Value.ToString();
+        }
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            _paused          = !_paused;
+            timer1.Enabled   = !_paused;
+            pauseButton.Text = _paused ? "\u25B6 Resume" : "\u23F8 Pause";
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            _paused          = false;
+            timer1.Enabled   = true;
+            pauseButton.Text = "\u23F8 Pause";
+            InitializeBalls(ballTrackBar.Value);
+        }
+    }
 }
